6725759cc4adc18f574328dde7e96f10c6654d1c
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-12-31 20:12:43 +0000

    rework queue method (doesnt work

--- src/main/java/me/bristermitten/packetlimiter/PacketLimiter.java
+++ src/main/java/me/bristermitten/packetlimiter/PacketLimiter.java
@@ -11,18 +11,20 @@ import com.comphenix.protocol.events.PacketEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
-import java.util.Queue;
+import java.util.Comparator;
+import java.util.NavigableSet;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentSkipListSet;
 
 public final class PacketLimiter extends JavaPlugin {
     private static final int DEFAULT_MAX_PACKETS = 15;
     private static final String DEBUG_PREFIX = "[Debug] ";
-    private final ConcurrentPlayerMap<Queue<PacketContainer>> packetQueue =
+    private final ConcurrentPlayerMap<NavigableSet<PacketContainer>> packetQueue =
             new ConcurrentPlayerMap<>(ConcurrentPlayerMap.PlayerKey.NAME);
     private final ConcurrentPlayerMap<Set<PacketContainer>> packetsSentInTick = new ConcurrentPlayerMap<>(ConcurrentPlayerMap.PlayerKey.NAME);
 
@@ -37,8 +39,16 @@ public final class PacketLimiter extends JavaPlugin {
         return packetsSentInTick.computeIfAbsent(player, p -> ConcurrentHashMap.newKeySet());
     }
 
-    private Queue<PacketContainer> getPacketQueue(Player player) {
-        return packetQueue.computeIfAbsent(player, p -> new ConcurrentLinkedQueue<>());
+    private NavigableSet<PacketContainer> getPacketQueue(Player player) {
+        return packetQueue.computeIfAbsent(player, p -> new ConcurrentSkipListSet<>(
+                Comparator.comparingDouble(packet -> {
+                    var x = packet.getIntegers().read(0);
+                    var z = packet.getIntegers().read(1);
+                    var pX = player.getLocation().getChunk().getX();
+                    var pZ = player.getLocation().getChunk().getZ();
+                    return new Vector(x, 0, z).distanceSquared(new Vector(pX, 0, pZ));
+                })
+        ));
     }
 
     @Override
@@ -55,7 +65,7 @@ public final class PacketLimiter extends JavaPlugin {
 
                 final Set<PacketContainer> packets = getPacketsSentInTick(event.getPlayer());
                 packets.add(event.getPacket());
-                final int maxPackets = getConfig().getInt("max-packets-per-tick", DEFAULT_MAX_PACKETS);
+                final int maxPackets = getMaxPackets();
                 if (packets.size() >= maxPackets) {
                     event.setCancelled(true);
                     debugLog("Cancelling chunk packet to player {} {}", event.getPlayer(), event.getPacket());
@@ -67,20 +77,26 @@ public final class PacketLimiter extends JavaPlugin {
         Bukkit.getScheduler().runTaskTimerAsynchronously(this, () -> {
             packetsSentInTick.values().forEach(Collection::clear);
             packetQueue.forEach((player, queue) -> {
-                int count = queue.size();
-                queue.forEach(packet -> {
+                int count = 0;
+                var packet = queue.pollFirst();
+                while (packet != null && count < getMaxPackets()) {
                     try {
                         debugLog("Resending packet {} to player {}", player, packet);
                         protocolManager.sendServerPacket(player, packet);
+                        count++;
                     } catch (InvocationTargetException e) {
                         e.printStackTrace();
                     }
-                });
+                    packet = queue.pollFirst();
+                }
                 if (count != 0) {
                     getSLF4JLogger().info("Resent {} packets to {}", count, player.getName());
                 }
-                queue.clear();
             });
         }, 0L, 1L);
+    }
+
+    private int getMaxPackets() {
+        return getConfig().getInt("max-packets-per-tick", DEFAULT_MAX_PACKETS);
     }
 }
